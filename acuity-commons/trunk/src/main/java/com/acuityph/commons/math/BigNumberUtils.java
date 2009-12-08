54fc398805924bd0525ba6bbb2ff7ab7f66cfd64
aisrael <aisrael@fe1f500a-a1d3-11de-ba4d-5dbaeb624ccf>
2009-12-08 08:04:27 +0000

    Added BigNumberUtils

---
+++ acuity-commons/trunk/src/main/java/com/acuityph/commons/math/BigNumberUtils.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2008-2009 Acuity Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created Dec 8, 2009
+ */
+package com.acuityph.commons.math;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Alistair A. Israel
+ */
+public final class BigNumberUtils {
+
+    /**
+     * Utility classes should not have a public or default constructor.
+     */
+    private BigNumberUtils() {
+        // noop
+    }
+
+    /**
+     * @param bigDecimal
+     *            a BigDecimal
+     * @return true if <code>bigDecimal</code> is not null and non-zero
+     */
+    public static boolean isNotNullOrZero(final BigDecimal bigDecimal) {
+        return bigDecimal != null && !BigDecimal.ZERO.equals(bigDecimal);
+    }
+}
---
+++ acuity-commons/trunk/src/test/java/com/acuityph/commons/math/BigNumberUtilsTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2008-2009 Acuity Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created Dec 8, 2009
+ */
+package com.acuityph.commons.math;
+
+import static com.acuityph.commons.math.BigNumberUtils.isNotNullOrZero;
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+/**
+ * JUnit test for {@link BigNumberUtils}.
+ *
+ * @author Alistair A. Israel
+ */
+public final class BigNumberUtilsTest {
+
+    /**
+     * Test for {@link BigNumberUtils#isNotNullOrZero(java.math.BigDecimal)} .
+     */
+    @Test
+    public void testIsNotNullOrZero() {
+        assertFalse(isNotNullOrZero(null));
+        assertFalse(isNotNullOrZero(BigDecimal.ZERO));
+        assertFalse(isNotNullOrZero(new BigDecimal(0)));
+        assertFalse(isNotNullOrZero(new BigDecimal(new BigInteger("0"), 0)));
+        assertTrue(isNotNullOrZero(BigDecimal.ONE));
+        assertTrue(isNotNullOrZero(new BigDecimal(2)));
+    }
+
+}
