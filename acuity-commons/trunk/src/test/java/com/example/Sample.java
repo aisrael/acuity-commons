de6933aa53c2121dac7261e8cd7f8218e4536ab2
aisrael@gmail.com <aisrael@gmail.com@fe1f500a-a1d3-11de-ba4d-5dbaeb624ccf>
2010-11-19 06:51:38 +0000

    Uh. Whoops.

--- acuity-commons/trunk/src/test/java/com/example/Sample.java
+++ acuity-commons/trunk/src/test/java/com/example/Sample.java
@@ -1,15 +1,19 @@
-/* Component: clpgas-utilities
+/**
+ * Copyright (c) 2008-2010 Acuity Technologies, Inc.
  *
- * Licensed Materials
- * Property of Chartis Insurance (Chartis Inc.)
- * A division of American International Group, Inc.
- * Copyright (c) 2010 American International Group, Inc.
- * All Rights Reserved
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Creation Date: Nov 9, 2010 6:54:18 PM
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * History
- * Nov 9, 2010    Initial creation        Alistair.Israel
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created Nov 9, 2010
  */
 package com.example;
 
