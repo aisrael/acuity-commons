cec4f31f8f079648234481ba350801f70c16395f
aisrael@gmail.com <aisrael@gmail.com@fe1f500a-a1d3-11de-ba4d-5dbaeb624ccf>
2010-11-22 13:49:13 +0000

    Spring -> 3.0.x, so acuity-commons -> 0.6

--- acuity-commons/trunk/src/test/java/com/acuityph/commons/util/ArrayUtilsTest.java
+++ acuity-commons/trunk/src/test/java/com/acuityph/commons/util/ArrayUtilsTest.java
@@ -17,7 +17,10 @@
  */
 package com.acuityph.commons.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
